8e866dfbc073282cea5d7fc810edb078b804b259
biswajit003 <58290167+biswajit003@users.noreply.github.com>
2019-11-28 14:37:03 +0530

    Add functionality got added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,13 @@ public class HelloWorld{
 public void display(){
 System.out.println("Hello World");
 }
+  public int add(int x,int y){
+    return x+y;
+  }
+
 public static void main(String []args){
 HelloWorld obj =new HelloWorld();
 obj.display();
+System.out.println(obj.add(3,4));
 }
 }
