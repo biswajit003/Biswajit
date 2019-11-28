be7fb7e5d55e8fb2af6faa2bba4660bc641be883
biswajit003 <58290167+biswajit003@users.noreply.github.com>
2019-11-28 14:33:04 +0530

    HelloWorld java file got created

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+public void display(){
+System.out.println("Hello World");
+}
+public static void main(String []args){
+HelloWorld obj =new HelloWorld();
+obj.display();
+}
+}
